1f9e224cdff3a7c4a8b8be6c96701f440d7fa63a
Maxim Dubovik <maks.dubovik.98@gmail.com>
2026-01-13 21:31:04 +0300

    Prepare Node.js server for Render deploy

--- server/index.js
+++ server/index.js
@@ -2,12 +2,19 @@ const http = require('http');
 const app = require('./app');
 const { Server } = require('socket.io');
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer(app);
-const io = new Server(server);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
 
 require('./sockets/game.socket')(io);
 require('./sockets/admin.socket')(io);
 
-server.listen(3000, () => {
-    console.log('server is running on port 3000');
+server.listen(PORT, () => {
+    console.log('server is running on port ${PORT}');
 });
